570640ccec44b68ebdb5770fdbb745aef1c3a64a
Vahagn0 <petrosyanvav@gmail.com>
2023-01-13 12:18:24 +0400

    questions getting from db

--- server/index.js
+++ server/index.js
@@ -1,42 +1,37 @@
 import express from "express"
 import cors from 'cors'
+import { MongoClient } from "mongodb"
 
-const app = express()
+  const client = new MongoClient("mongodb://localhost:27017")
 
-app.use(cors())
+  async function a(){
+    await client.connect()
 
-  const questions = [
-        {
-          question: "What's the capital of France ?",
-          answer: [
-            { answerText: "Moscow", isTrue: false },
-            { answerText: "Paris", isTrue: true },
-            { answerText: "Berlin", isTrue: false },
-            { answerText: "Antananarivu", isTrue: false },
-          ],
-        },
-        {
-          question: "Who was first president of USA ?",
-          answer: [
-            { answerText: "Harry S. Truman", isTrue: false },
-            { answerText: "Bill Clinton", isTrue: false },
-            { answerText: "George Washington", isTrue: true },
-            { answerText: "Ronald Reagan", isTrue: false },
-          ],
-        },
-        {
-          question: "Whats the oldest country ?",
-          answer: [
-            { answerText: "Japan", isTrue: false },
-            { answerText: "Vietnam", isTrue: false },
-            { answerText: "Egypt", isTrue: true },
-            { answerText: "Armenia", isTrue: false },
-          ],
-        },
-      ];
+    const cleanup = (evt) =>{
+      client.close()
+      process.exit()
+    }
+    
+    process.on("SIGINT",cleanup)
+    process.on("SIGTERM",cleanup)
 
-    app.get("/",(req,res)=>{
-        res.send(questions)
-    })
+    const db = client.db("quiz")
+    const collection = db.collection("questions")
 
-app.listen(3001)
+
+    //express
+
+    const app = express()
+
+    app.use(cors())
+
+        app.get("/", async (req,res)=>{
+            const questions = await collection.find({}).toArray()
+            res.send(questions)
+        })
+
+    app.listen(3001)
+
+  }
+  a()
+
